68244640ece5ed228d6acb58986b66dca228ff5b
Tongre001 <40128067+Tongre001@users.noreply.github.com>
2018-10-30 14:44:54 -0700

    Update mysqlconnect.php
    This file is to establish a connection to your MYSQL database. This is connected and used in the about.html file, it can be found executed within the form 'action' field.

--- mysqlconnect.php
+++ mysqlconnect.php
@@ -1,14 +1,15 @@
 <?php
-$server= "localhost";
-$user = "amendiet_root";
-$pass = "100%EmptyRoot";
-$dbname = "amendiet_forms";
+$server= "localhost";   //Your server
+$user = " ";		//Your username
+$pass = " ";		//Use your password, if you don't have a password leave it empty
+$dbname = " ";		//YOur database name
 //creates connection to mysql.
 $con = mysqli_connect($server, $user, $pass, $dbname);
 
 //checks connection 
 if(!$con){
 	die("Can not connect: " . mysqli_connect_error());
+//if connection is successful continue to else
 }else{
 	$name = mysqli_real_escape_string($con, $_POST['name']);
 	$company = mysqli_real_escape_string($con, $_POST['company']);
@@ -16,6 +17,7 @@ if(!$con){
 	$message = mysqli_real_escape_string($con, $_POST['message']);
 	$inquery = mysqli_real_escape_string($con, $_POST['inquery']);
 
+	//inserts the variables the SQL QUERY
 	$sql = "INSERT INTO forms (name, company, email, message, inquery) 
 	VALUES ('$name','$company','$email','$message','$inquery')";
 
